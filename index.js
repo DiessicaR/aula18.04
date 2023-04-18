d9af3a305a6ca288ca1dd2e349f41df5c4a8fe7a
Diessica <diessicarrc@gmail.com>
2023-04-18 19:52:41 -0300

    exercicios array, length include push pop splice

---
+++ index.js
@@ -0,0 +1,18 @@
+//Exemplo Slide
+const letras = ["A","B","C","D","E","F","G","H"]
+letras.splice(2, 1)
+
+//Para este exercicio, comece criando um array com os valores: 1, 2, 3, 4, 5, 6
+const numeros = [1,2,3,4,5,6]
+
+//Determine o tamanho do array
+alert(numeros.length)
+
+//Adicione o número 7
+alert(numeros.push(7))
+
+//Remova os números 4 e 5
+numeros.splice(3,2)
+
+//Determine o novo tamanho do array 
+alert(numeros.length)
